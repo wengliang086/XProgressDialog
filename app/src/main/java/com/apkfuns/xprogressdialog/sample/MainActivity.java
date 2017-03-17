8394073820098f384b2bec8a985f451dce51c623
liyongxiang <liyongxiang@hoolai.com>
2017-03-17 13:27:54 +0800

    改 style

--- app/src/main/java/com/apkfuns/xprogressdialog/sample/MainActivity.java
+++ app/src/main/java/com/apkfuns/xprogressdialog/sample/MainActivity.java
@@ -33,13 +33,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.click1) {
-            dialog = new XProgressDialog(this, "正在加载正在加载正在加载正在加载..", XProgressDialog.THEME_HORIZONTAL_SPOT);
+            dialog = new XProgressDialog(this, "数据加载中...", XProgressDialog.THEME_HORIZONTAL_SPOT);
         } else if (v.getId() == R.id.click2) {
-            dialog = new XProgressDialog(this, "正在加载正在加载正在加载正在加载..", XProgressDialog.THEME_CIRCLE_PROGRESS);
+            dialog = new XProgressDialog(this, "数据加载中...", XProgressDialog.THEME_CIRCLE_PROGRESS);
         } else if (v.getId() == R.id.click3) {
-            dialog = new XProgressDialog(this, "正在加载正在加载正在加载正在加载..", XProgressDialog.THEME_HEART_PROGRESS);
+            dialog = new XProgressDialog(this, "数据加载中...", XProgressDialog.THEME_HEART_PROGRESS);
         }
-        dialog.setMessage("1232123213");
+//        dialog.setMessage("1232123213");
         dialog.show();
 
         handler.sendMessageDelayed(new Message(), 5000L);
